87288d09b52f8ed335a706c77b03118eb089cfb3
devpabloh <devpabloh@gmail.com>
2026-04-08 16:19:53 -0300

    "feat: Adicionando uma orientação dois ao componente inputComponent"

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,15 +1,17 @@
-interface InputComponent extends React.InputHTMLAttributes<HTMLInputElement>{
+interface InputComponent extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string
   classNameLabel?: string
   orientacoes?: string
+  segundaOrientacao?: string
 }
 
-export function InputComponent({label,classNameLabel,orientacoes, ...rest}:InputComponent){
-  return(
+export function InputComponent({ label, classNameLabel, orientacoes, segundaOrientacao, ...rest }: InputComponent) {
+  return (
     <div className="my-4">
       <label htmlFor={rest.id} className={`font-semibold ${classNameLabel}`}>{label}</label>
       <p className="orientacoes mb-2" >{orientacoes}</p>
-      <input id={rest.id} className='border rounded-sm p-2 w-full' {...rest}/>
+      <p className="orientacoes">{segundaOrientacao}</p>
+      <input id={rest.id} className='border rounded-sm p-2 w-full' {...rest} />
     </div>
   )
 }
